8749e3d9285ce3fa2ea064d7adebd5af10b4dcdc
MisterTakaashi <soapmctravich@gmail.com>
2017-12-14 17:45:52 +0100

    Inclusion bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { RoutingModule } from './app.router';
+
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,7 @@ import { AboutComponent } from './pages/about/about.component';
     BrowserModule.withServerTransition({ appId: 'universal-demo-v5' }),
     HttpClientModule,
     BrowserTransferStateModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
-      { path: '**', redirectTo: '/' }
-    ])
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
