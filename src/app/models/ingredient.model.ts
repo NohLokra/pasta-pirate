cf3aeaa1926440f32c33e43dea240ac8edf112e3
MisterTakaashi <soapmctravich@gmail.com>
2017-12-22 20:51:45 +0100

    Gestion de stock pour recette

--- src/app/models/ingredient.model.ts
+++ src/app/models/ingredient.model.ts
@@ -3,4 +3,5 @@ import { AlimentModel } from './aliment.model';
 export class IngredientModel {
   aliment: AlimentModel;
   quantity: number;
+  isInStock?: boolean = false;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { StockService } from '../../services/stock.service';
 import { StockModel } from '../../models/stock.model';
 import { RecipeService } from '../../services/recipe.service';
 import { RecipeModel } from '../../models/recipe.model';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +16,8 @@ export class HomeComponent implements OnInit {
   stocks: StockModel[];
   recipe: RecipeModel;
 
+  recipeStock: any[];
+
   constructor(
     private _stockService: StockService,
     private _recipeService: RecipeService
@@ -25,12 +28,22 @@ export class HomeComponent implements OnInit {
       this.stocks = data;
     });
 
+    this.recipeStock = [];
+
     this.selectRecipe("5qzd46zqgq8q46f4zq68fqz");
   }
 
   selectRecipe(id: string) {
     this._recipeService.getRecipe(id).subscribe(data => {
       this.recipe = data;
+
+      this.recipe.ingredients.forEach(ingredient => {
+        let alimentStock = this.stocks.find(x => x.aliment.id == ingredient.aliment.id);
+
+        if (alimentStock && alimentStock.quantity >= ingredient.quantity) {
+          ingredient.isInStock = true;
+        }
+      });
     });
   }
 }
